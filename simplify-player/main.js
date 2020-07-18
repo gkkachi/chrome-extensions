8f153607b75b31fe0d7076bf56ab4ccc37e076b1
Konosuke Kachi <github@s.k-kachi.net>
2020-07-19 02:56:16 +0900

    Update for schoo

--- simplify-player/main.js
+++ simplify-player/main.js
@@ -3,26 +3,26 @@ function main () {
 
     let ids = [];
     let classNames = [];
-    let nodeToFullScreen = null;
+    let nodeToFullWidth = null;
 
     if (hostName === 'schoo.jp') {
-        ids = ['room_sub_mod', 'roomHeader'];
+        ids = ['room_sub_mod', 'roomHeader', 'head_bar_mod', 'tools_mod', 'desk_mod'];
         classNames = ['twinkle01', 'twinkle02', 'actionNavi'];
-        nodeToFullScreen = document.getElementById('screen');
+        nodeToFullWidth = document.getElementById('screen') || document.getElementById('roomInner');
     } else {
         console.log(`Does not support this site: ${hostName}`);
     }
 
     const nodesToRemove = [
         ids.map(x => document.getElementById(x)),
-        classNames.map(x => document.getElementsByClassName(x)[0]),
+        classNames.map(x => document.getElementsByClassName(x)).map(x => x.lengh > 0 ? x[0] : null)
     ];
 
-    nodesToRemove.flat().forEach(x => x.remove && x.remove());
-    toFullScreen(nodeToFullScreen);
+    nodesToRemove.flat().filter(x => x && x.remove).forEach(x => x.remove());
+    nodeToFullWidth && toFullWidth(nodeToFullWidth);
 }
 
-function toFullScreen(node) {
+function toFullWidth(node) {
     node.style.width = '100vw';
     node.style.margin = '0px';
     node.style.padding = '0px';
