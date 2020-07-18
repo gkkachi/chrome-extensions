fde98b236b2a876b288453e2b61326fc764521a1
Konosuke Kachi <github@s.k-kachi.net>
2020-07-19 06:47:33 +0900

    Add youtube

--- simplify-player/main.js
+++ simplify-player/main.js
@@ -3,12 +3,18 @@ function main () {
 
     let ids = [];
     let classNames = [];
-    let nodeToFullWidth = null;
+    let nodesToFullWidth = [];
 
     if (hostName === 'schoo.jp') {
         ids = ['room_sub_mod', 'roomHeader', 'head_bar_mod', 'tools_mod', 'desk_mod'];
         classNames = ['twinkle01', 'twinkle02', 'actionNavi'];
-        nodeToFullWidth = document.getElementById('screen');
+        nodesToFullWidth = [document.getElementById('screen')];
+    } else if (hostName === 'www.youtube.com') {
+        ids = ['masthead-container', 'items', 'info', 'meta', 'ticket-shelf', 'merch-shelf', 'comments'];
+        nodesToFullWidth = [
+           document.getElementsByTagName('video')[0],
+           document.getElementById('ytd-player'),
+        ];
     } else {
         console.log(`Does not support this site: ${hostName}`);
     }
@@ -19,16 +25,20 @@ function main () {
     ];
 
     nodesToRemove.flat().filter(x => x).forEach(x => x.remove());
-    nodeToFullWidth && toFullWidth(nodeToFullWidth);
+    nodesToFullWidth.map(toFullWidth);
 
     // Refresh
     window.dispatchEvent(new Event('resize'));
 }
 
 function toFullWidth(node) {
+    node.style.position = 'fixed';
+    node.style.top = '0px';
+    node.style.left = '0px';
     node.style.width = '100vw';
+    node.style.height = 'calc(min(100vw * 9 / 16, 100vh))';
     node.style.margin = '0px';
     node.style.padding = '0px';
 }
 
-main();
+setInterval(main, 1000);
