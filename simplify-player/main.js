8fea52293d0e940e4ec2ef2f1ce5fa46c6bb876d
Konosuke Kachi <github@s.k-kachi.net>
2020-07-19 05:22:35 +0900

    Update

--- simplify-player/main.js
+++ simplify-player/main.js
@@ -8,17 +8,17 @@ function main () {
     if (hostName === 'schoo.jp') {
         ids = ['room_sub_mod', 'roomHeader', 'head_bar_mod', 'tools_mod', 'desk_mod'];
         classNames = ['twinkle01', 'twinkle02', 'actionNavi'];
-        nodeToFullWidth = document.getElementById('screen') || document.getElementById('roomInner');
+        nodeToFullWidth = document.getElementById('screen');
     } else {
         console.log(`Does not support this site: ${hostName}`);
     }
 
     const nodesToRemove = [
         ids.map(x => document.getElementById(x)),
-        classNames.map(x => document.getElementsByClassName(x)).map(x => x.lengh > 0 ? x[0] : null)
+        classNames.map(x => document.getElementsByClassName(x)).map(x => [...x]).flat()
     ];
 
-    nodesToRemove.flat().filter(x => x && x.remove).forEach(x => x.remove());
+    nodesToRemove.flat().filter(x => x).forEach(x => x.remove());
     nodeToFullWidth && toFullWidth(nodeToFullWidth);
 
     // Refresh
